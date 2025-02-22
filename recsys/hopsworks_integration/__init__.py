76d36c8d642fb100034d40574748deb837d7b52f
Shanilka Haturusinghe <s.haturusinghe99@gmail.com>
2025-02-22 14:38:13 +0000

    Add two_tower_serving module and update __init__.py for integration

--- recsys/hopsworks_integration/__init__.py
+++ recsys/hopsworks_integration/__init__.py
@@ -1,4 +1,4 @@
-from . import feature_store
+from . import feature_store, two_tower_serving
 from .feature_store import get_feature_store
 
-__all__ = ["feature_store", "get_feature_store"]
+__all__ = ["feature_store", "get_feature_store", "two_tower_serving"]
